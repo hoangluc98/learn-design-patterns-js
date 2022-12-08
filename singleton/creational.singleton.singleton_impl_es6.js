09f611b166ebc0feaba1b271907e68cba9797243
luc.hoang <luc.hoang@savvycomsoftware.com>
2022-12-08 17:58:10 +0700

    singleton

---
+++ singleton/creational.singleton.singleton_impl_es6.js
@@ -0,0 +1,14 @@
+
+class Singleton {
+  constructor() {
+    const instance = this.constructor.instance
+    if (instance) {
+      return instance
+    }
+
+    this.constructor.instance = this
+  }
+}
+
+let s1 = new Singleton()
+let s2 = new Singleton()
